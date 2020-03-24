0d8ea474d2b199bb895f54b19f163c8b45b64141
Alice Wang <aywang6@gmail.com>
2020-03-24 22:56:32 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -140,7 +140,7 @@ def adding_to_dennis
       }
     }
     
-    programmer_hash[:dennis_ritchie][:languages] 
+    programmer_hash[:dennis_ritchie][:languages].pop("Assembly") 
     return programmer_hash
 
 end
